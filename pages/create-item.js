f13d7d3ca3fe5252036edca7fb4e27cf761a0a57
vvronskyFX <vronskymail@gmail.com>
2022-04-22 17:37:30 -0400

    Slight code additions

--- pages/create-item.js
+++ pages/create-item.js
@@ -105,7 +105,7 @@ export default function CreateItem() {
           )
         }
         <button onClick={createMarket} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
-          Create Digital Asset
+          Create NFT
         </button>
       </div>
     </div>
--- pages/my-assets.js
+++ pages/my-assets.js
@@ -2,6 +2,7 @@ import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from "web3modal"
+import { useRouter } from 'next/router'
 
 import {
   nftmarketaddress, nftaddress
@@ -13,6 +14,7 @@ import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
 export default function MyAssets() {
   const [nfts, setNfts] = useState([])
   const [loadingState, setLoadingState] = useState('not-loaded')
+  const router = useRouter()
   useEffect(() => {
     loadNFTs()
   }, [])
@@ -43,7 +45,10 @@ export default function MyAssets() {
     setNfts(items)
     setLoadingState('loaded') 
   }
-  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets owned</h1>)
+  function listNFT(nft) {
+    router.push(`/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}`)
+  }
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No NFTs owned</h1>)
   return (
     <div className="flex justify-center">
       <div className="p-4">
@@ -54,6 +59,7 @@ export default function MyAssets() {
                 <img src={nft.image} className="rounded" />
                 <div className="p-4 bg-black">
                   <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
+                  <button className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => listNFT(nft)}>List</button>
                 </div>
               </div>
             ))
